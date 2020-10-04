c8834ca635e21e77a61fa0513ff50361fad8f7a0
jonnyv91 <vaflorjonathan@johndeere.com>
2020-10-04 13:01:22 -0500

    test changes

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router= express.Router();
 const ctrl = require('../controllers');
 
-
-
-
+// router.get('/signup', ctrl.auth.renderSignup)
+// router.post('/signup', ctrl.auth.createUser)
+// router.get('/login', ctrl.auth.renderLogin)
+// router.post('/login', ctrl.auth.checkUser)
 
 
 module.exports = router;
