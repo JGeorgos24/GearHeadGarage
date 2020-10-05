20befb80b1551b3b743c278a59c4768f8d89c09b
jonnyv91 <vaflorjonathan@johndeere.com>
2020-10-05 10:19:14 -0500

    added login render

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,98 @@
-require('dotenv').config();
-
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-
+// Import Drivers model
 const Driver = require('../models').Driver;
 
+// Import BCRYPT 
+const bcrypt = require('bcryptjs');
 
+// used for JWT
+require('dotenv').config()
+const jwt = require('jsonwebtoken');
 
+// shows signup page
+const renderSignup = (req, res) => {
+    Driver.findByPk(req.params.index)
+        .then(user => {
+            res.render('drivers/register.ejs', {
+                user: user
+            });
+        })
+}
+// shows login page
+const renderLogin = (req, res) => {
+    res.render('drivers/login.ejs');
+}
+
+const createUser = (req, res) => {
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            return res.send('err');
+        }
+        bcrypt.hash(req.body.password, salt, (err, hashedPwd) => {
+            if (err) {
+                return res.send('err');
+            }
+            req.body.password = hashedPwd;
+
+            Driver.create(req.body)
+                .then(newDriver => {
+                    // Assinging payload to token
+                    const token = jwt.sign(
+                        {
+                            id: newDriver.id,
+                            username: newDriver.username
+                        },
+                        // Secret key to encode token
+                        process.env.JWT_SECRET,
+                        // option to at time
+                        {
+                            expiresIn: '30 days'
+                        }
+                    );
+                    console.log(token);
+                    res.cookie('jwt', token);
+                    //once added to cookie, direct user to profile page
+                    res.redirect(`/users/profile/${newDriver.id}`);
+                })
+
+        })
+    })
+}
+
+const checkUser = (req, res) => {
+    Driver.findOne({
+        where: {
+            username: req.body.username
+        }
+    })
+        .then(foundDriver => {
+            if (foundDriver) {
+                bcrypt.compare(req.body.password, foundDriver.password, (err, match) => {
+                    if (match) {
+                        const token = jwt.sign(
+                            {
+                                username: foundDriver.username,
+                                id: foundDriver.id
+                            },
+                            process.env.JWT_SECRET,
+                            {
+                                expiresIn: "30 days"
+                            },
+                        );
+                        res.cookie('jwt', token)
+                        res.redirect(`/drivers/profile`);
+                    } else {
+                        res.send('Incorrect Password')
+                    }
+                });
+            }
+
+        })
+}
 
 module.exports = {
+
+    renderSignup,
+    renderLogin,
+    createUser,
+    checkUser
 }
--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const ctrl = require('../controllers');
 
 // router.get('/signup', ctrl.auth.renderSignup)
 // router.post('/signup', ctrl.auth.createUser)
-// router.get('/login', ctrl.auth.renderLogin)
+router.get('/login', ctrl.auth.renderLogin)
 // router.post('/login', ctrl.auth.checkUser)
 
 
